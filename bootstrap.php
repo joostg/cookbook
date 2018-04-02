558b7856652431ab13ad2d9f38379eb5ec5880e0
Joost <joost.ganzeveld@gmail.com>
2018-04-02 12:16:03 +0200

    fixed bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($classname) {
-	$file = __DIR__ . '/../' . str_replace('\\','/',str_replace('cookbook\\','',$classname)) . '.php';
+	$file = __DIR__ . '/' . str_replace('\\','/',str_replace('cookbook\\','',$classname)) . '.php';
 
 	if (!file_exists($file)) {
 		return false;
